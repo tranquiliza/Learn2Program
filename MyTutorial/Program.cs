4e1fbe81fd1cb13ec2f942763d0783f7bd599375
Daniel Olsen <daniel.olsen.server@gmail.com>
2019-07-27 19:50:23 +0200

    Add project files.

---
+++ MyTutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _rnd = new Random();
+
+            var number = _rnd.Next();
+
+            Console.WriteLine(number);
+            Console.Read();
+        }
+    }
+}
